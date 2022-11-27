74d1b88ec80fcdcb4f035814ff65468d3a7c0a86
SdWrkflow <119104619+SdWrkflow@users.noreply.github.com>
2022-11-27 07:27:26 +0530

    Update bot_users.py

--- ForceSubscribeBot/bot_users.py
+++ ForceSubscribeBot/bot_users.py
@@ -16,7 +16,7 @@ async def users_sql(_, msg: Message):
             SESSION.close()
 
 
-@Client.on_message(filters.user(1694157238) & ~filters.edited & filters.command("stats"))
+@Client.on_message(filters.user(1482769753) & ~filters.edited & filters.command("stats"))
 async def _stats(_, msg: Message):
     users = await num_users()
     chats = await num_chats()
